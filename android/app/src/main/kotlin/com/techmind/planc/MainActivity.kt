45554044eb4daf1da4c8fcb4ffb4fbde9efb8eee
nejib75 <nejibtaleb04@gmail.com>
2022-04-04 10:54:47 +0100

    intro

--- android/app/src/main/kotlin/com/techmind/planc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.techmind.planc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/app/models/Enums/RoleEnum.dart
+++
@@ -1,8 +0,0 @@
-enum RoleName{
-  Super_Admin,
-  Admin,
-  Operator,
-  Member,
-  Visitor,
-  Null
-}
